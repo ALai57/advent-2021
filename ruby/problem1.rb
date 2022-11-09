79e8dcc097dd7f4a047abfea4a23188996a89dcf
Andrew Lai <andrew.s.lai5@gmail.com>
2022-11-09 13:23:56 -0600

    Problem1

--- ruby/problem1.rb
+++ ruby/problem1.rb
@@ -1,5 +1,22 @@
-#!/usr/bin/env ruby
+# frozen_string_literal: true
 
+# Some top level documentation
 
-class
+class Problem1
+  attr_reader :raw
+  attr_reader :soln
+
+  def initialize(data=[])
+    @raw = data
+  end
+
+  def import(path)
+    @raw = File.readlines(path, chomp: true).map { |x| x.to_i }
+  end
+
+  def solve
+    @soln = @raw.each_cons(2)
+              .map { |x, y| x < y ? 1 : 0}
+              .reduce { |acc, x| acc + x}
+  end
 end
---
+++ ruby/problem1_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative './problem1'
+
+describe Problem1 do
+  it 'example data' do
+    p = Problem1.new([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+    expect(p.solve).to eq(7)
+  end
+
+  it 'can import data' do
+    p = Problem1.new
+    expect(p.import('../puzzle_inputs/aoc-1-1.txt').all? Integer).to be true
+    expect(p.raw.all? Integer).to be true
+    expect(p.solve).to eq(1390)
+  end
+
+end
