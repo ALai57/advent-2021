f35d370a63382850b28f3a8cc6a34ed8d9975098
Andrew Lai <andrew.s.lai5@gmail.com>
2022-11-09 21:23:50 -0600

    Puzzle 2

--- ruby/p2.rb
+++ ruby/p2.rb
@@ -13,8 +13,35 @@ def step(acc, command)
   acc.merge(update) { |k, old, new| old + new}
 end
 
-raw = File.readlines('../puzzle_inputs/aoc-2-ex.txt', chomp: true)
+ex = File.readlines('../puzzle_inputs/aoc-2-ex.txt', chomp: true)
         .map(&:split)
         .map { |dir, n| [dir, n.to_i]}
 
-raw.reduce({"horizontal" => 0, "depth" => 0}) { |acc, command| step(acc, command)}
+ex.reduce({"horizontal" => 0, "depth" => 0}) { |acc, command| step(acc, command)}
+
+
+raw = File.readlines('../puzzle_inputs/aoc-2.txt', chomp: true)
+        .map(&:split)
+        .map { |dir, n| [dir, n.to_i]}
+
+result = raw.reduce({"horizontal" => 0, "depth" => 0}) { |acc, command| step(acc, command)}
+result["horizontal"] * result['depth']
+
+
+
+def step2(acc, command)
+  dir, n = command
+  update = case dir
+           when 'forward'
+             {'horizontal' => n, 'depth' => acc['aim'] * n}
+           when 'down'
+             {'aim' => n}
+           when 'up'
+             {'aim' => -n}
+           end
+
+  acc.merge(update) { |k, old, new| old + new}
+end
+
+result2 = raw.reduce({"horizontal" => 0, "depth" => 0, "aim" => 0}) { |acc, command| step2(acc, command)}
+result2["horizontal"] * result2['depth']
