1c500f02f9a0ae6abce2343dc69bd565df61c920
Andrew Lai <andrew.s.lai5@gmail.com>
2022-11-14 23:03:39 -0600

    Rubyin

--- ruby/README.md
+++ ruby/README.md
@@ -3,3 +3,15 @@
 # https://github.com/gchan/advent-of-code-ruby/blob/master/2021/
 
 # https://github.com/Bogdanp/awesome-advent-of-code/blob/master/2021.md#ruby
+
+# Debugging
+https://emacs-lsp.github.io/dap-mode/page/configuration/#ruby
+
+https://github.com/rubyide/vscode-ruby/wiki/1.-Debugger-Installation
+
+gem install ruby-debug-ide
+gem install debase -v '0.2.5.beta2'
+
+dap-breakpoint-add
+dap-breakpoint-toggle
+dap-ui-repl
--- ruby/p3.rb
+++ ruby/p3.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 
 ex = File.readlines('../puzzle_inputs/aoc-3-ex.txt', chomp: true)
-       .map { |x| x.split("")}
+       .map { |x| x.split("").map(&:to_i) }
 
 def frequencies(arr)
   arr.group_by { |el| el }
@@ -17,7 +17,7 @@ def most_common(arr)
 end
 
 def least_common(arr)
-  frequencies(arr).reverse![0][0]
+  frequencies(arr).reverse[0][0]
 end
 
 def gamma(xs)
@@ -30,28 +30,42 @@ def epsilon(xs)
     .join.to_i(2)
 end
 
-most_common([1, 2, 3, 1, 1, 1, 2, 1, 4]) ## => 1
+# most_common([1, 2, 3, 1, 1, 1, 2, 1, 4]) ## => 1
 
 gamma(ex) * epsilon(ex)
 
 
 raw = File.readlines('../puzzle_inputs/aoc-3.txt', chomp: true)
-        .map { |x| x.split("")}
+        .map { |x| x.split("").map(&:to_i) }
 
 gamma(raw) * epsilon(raw)
 
 def rating(arr)
-  x = arr.clone
-  bits = []
-  (0..x[0].size-1).each do |i|
-    puts "i #{i}. bits #{bits}"
-    bit = yield x.transpose[i]
-    x.select! { |arr| arr[i] == bit }
-    bits[i] = bit
+  n_bits = arr[0].size-1
+  (0..n_bits).reduce(arr.clone) do |bit_array, bit_index|
+    #puts "Bit Index #{bit_index}. Bit Array #{bit_array}"
+    if bit_array.size == 1
+      return bit_array
+    else
+      bit = yield bit_array.transpose[bit_index]
+      bit_array.select { |xs| xs[bit_index] == bit }
+    end
   end
-
-  bits.join.to_i(2)
 end
 
-rating(ex) { |bit_array| most_common(bit_array)}
-rating(ex) { |bit_array| least_common(bit_array)}
+def mean(arr)
+  arr.sum(0.0) / arr.size
+end
+
+def o2(bit_array)
+  rating(bit_array) { |arr| mean(arr) >= 0.5 ? 1 : 0 }
+    .join.to_i(2)
+end
+
+def co2(bit_array)
+  rating(bit_array) { |arr| mean(arr) >= 0.5 ? 0 : 1 }
+    .join.to_i(2)
+end
+
+o2(ex) * co2(ex)
+o2(raw) * co2(raw)
